a6bfa0f686b93eba2c0d061288568a74244c4109
rsalo1981 <55482188+rsalo1981@users.noreply.github.com>
2021-06-11 23:13:13 +0200

    first commit

---
+++ ViewModels/ProductUserVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSMachine_Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IList<Product> ProductList { get; set; }
+    }
+}
